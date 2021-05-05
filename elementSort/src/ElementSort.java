d79f2581565b2b5566dd1c2a971292e614e4e759
HAORAN DENG <haorandeng@haorans-mbp.lan>
2021-05-05 15:15:00 -0400

    element sort

---
+++ elementSort/src/ElementSort.java
@@ -0,0 +1,77 @@
+import java.util.Arrays;
+
+public class ElementSort {
+
+    public static void selectionSort(Comparable[] a) {
+
+        int N = a.length;
+        for (int i = 0; i < N; i++) {
+            int min = i;
+            for (int j = i + 1; j < N; j++) {
+                if (less(a[j], a[min])) {
+                    min = j;
+                }
+            }
+            exch(a, min, i);
+
+        }
+    }
+
+    public static void insertionSort(Comparable[] a) {
+        int N = a.length;
+
+        for (int i = 0; i < N; i++) {
+
+            for (int j = i; j > 0; j--) {
+                if (less(a[j], a[j - 1])) {
+                    exch(a, j, j - 1);
+                } else break;
+            }
+        }
+    }
+
+    public static void hSort(Comparable[] a) {
+        int N = a.length;
+        int h = 1;
+
+        while (h < N / 3) h = 3 * h + 1;
+
+        while (h >= 1) {
+            for (int i = h; i < N; i++) {
+                for (int j = i; j >= h; j--) {
+                    if (less(a[j], a[j - h])) {
+                        exch(a, j, j - h);
+                    }
+                }
+            }
+            h = h / 3;
+
+        }
+    }
+
+    private static boolean less(Comparable v, Comparable w) {
+
+        return v.compareTo(w) < 0;
+    }
+
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable swap = a[i];
+        a[i] = a[j];
+        a[j] = swap;
+
+
+    }
+
+    public static void main(String[] args) {
+        Integer[] a = {1, 3, 2, 4, 1};
+        ElementSort.hSort(a);
+
+
+        for (int aa : a) {
+            System.out.println(aa);
+        }
+
+        Arrays.sort(a);
+
+    }
+}
